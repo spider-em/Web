eaaf34686742bfefe063a1623634ced8824ceb0e
ArDean Leith <leith@wadsworth.org>
2015-07-17 11:14:39 -0400

    wantmsg parameter, output formatting

--- src/tiltang.c
+++ src/tiltang.c
@@ -1,52 +1,85 @@
 
-/*$Header: /usr8/web/src/RCS/tiltang.c,v 1.13 2011/06/22 19:06:36 leith Exp $*/
+/*$Header: /usr8/web/src/RCS/tiltang.c,v 1.14 2015/06/11 13:27:53 leith Exp $*/
 
-/********************************************************************
-C
-C   tiltang(x0,y0,xt,yt,npoint,theta,iarea,b)
-C
-C   PURPOSE:  Calculate the tiltangle between two images
-C
-C   CALLED BY: 
-C
-C   RETURNS:  0   OK
-C             1   Not enough locations or area betweeen locations
-C            -1   Some locations are bad
-C  
-C   ALSO RETURNS:  theta      Computed tilt angle
-C                             (Theta only altered if ok)
-C                  iarea      Number of tiangles used to get tilt             
-C                         
-*********************************************************************/
+/*
+ ***********************************************************************
+ C                                                                     *
+ C tiltang.c                                                           *
+ C               Improved                        Jun 2015 ArDean Leith *
+ C**********************************************************************
+ C=* FROM: WEB - VISUALIZER FOR SPIDER MODULAR IMAGE PROCESSING SYSTEM *
+ C=* Copyright (C) 1992-2015  Health Research Inc.                     *
+ C=*                                                                   *
+ C=* HEALTH RESEARCH INCORPORATED (HRI),                               *   
+ C=* ONE UNIVERSITY PLACE, RENSSELAER, NY 12144-3455.                  *
+ C=*                                                                   *
+ C=* Email:  spider@wadsworth.org                                      *
+ C=*                                                                   *
+ C=* This program is free software; you can redistribute it and/or     *
+ C=* modify it under the terms of the GNU General Public License as    *
+ C=* published by the Free Software Foundation; either version 2 of    *
+ C=* the License, or (at your option) any later version.               *
+ C=*                                                                   *
+ C=* This program is distributed in the hope that it will be useful,   *
+ C=* but WITHOUT ANY WARRANTY; without even the implied warranty of    *
+ C=* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU *
+ C=* General Public License for more details.                          *
+ C=*                                                                   *
+ C=* You should have received a copy of the GNU General Public License *
+ C=* along with this program; if not, write to the                     *
+ C=* Free Software Foundation, Inc.,                                   *
+ C=* 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.     *
+ C=*                                                                   *
+ ***********************************************************************
+ C
+ C   tiltang(x0,y0,xt,yt,npointss,theta,iarea,arealim,wantmsg)
+ C
+ C   PURPOSE:  Calculate the tiltangle between two images
+ C
+ C   CALLED BY: 
+ C
+ C   RETURNS:  0   OK
+ C             1   Not enough locations or area betweeen locations
+ C            -1   Some locations are bad
+ C  
+ C   ALSO RETURNS:  theta      Computed tilt angle
+ C                             (Theta only altered if ok)
+ C                  iarea      Number of tiangles used to get tilt             
+ C                         
+ **********************************************************************
+*/
 
-#include "std.h"
+#include "common.h"
 #include "routines.h"
 
 int tiltang(float *x0, float *y0, float *xt, float *yt,
-	     int npoint, float * theta, int * iarea, float arealim)
+	    int npoints, float * theta, 
+            int * iarea, float arealim, int wantmsg)
 
 {
-char	outmsg[80];
-int	i, k, ntot, iflag;
-float	area0, areat, w, sum, temp;
-float   x01, x02, y01, y02, xt1, xt2, yt1, yt2;
+char	    outmsg[80];
+int	    i, k, ntot, iflag;
+float	    area0, areat, w, sum, temp;
+float       x01, x02, y01, y02, xt1, xt2, yt1, yt2;
 
-const float pi = 3.14159;
+const       float pi = 3.14159;
 
 sum    = 0.0;
 *iarea = 0;
 ntot   = 0;
 
-if (npoint < 3)
+if (npoints < 3)
    { 
-   spout(" *** Unable to compute Theta; Need > 2 points!");
+   sprintf(outmsg,"*** Unable to compute tilt (theta); Have only: %d points.  Need > 2 points",npoints); 
+   spout(outmsg); 
+   XBell(idispl,50);
    return 1;
    }
 
 iflag = 0;
-for (i = 0; i < npoint; i++)
+for (i = 0; i < npoints; i++)
     {
-    for (k = i + 1; k < npoint-1; k++)
+    for (k = i + 1; k < npoints-1; k++)
 	{
         /* Area in untilted image: */
 	x01    = x0[k]   - x0[i];
@@ -130,17 +163,19 @@ for (i = 0; i < npoint; i++)
 
 if (*iarea == 0) 
    {
-   spout(
-    " *** Unable to compute tilt angle; Need 3 points with area > arealim!");
+   spout(" *** Unable to compute tilt angle; Need > 2 points with area > arealim!");
    return 1;
    }
 
 *theta = sum    / (*iarea);
 *theta = *theta / pi * 180.0;
 
-sprintf(outmsg, "Areas used for theta: %d, out of possible: %d", 
-                 *iarea,ntot);
-spout(outmsg); 
+if (wantmsg)
+   {
+   sprintf(outmsg, "Points: %d   Areas used for theta: %d, out of possible: %d", 
+                 npoints, *iarea, ntot);
+   spout(outmsg);
+   } 
 
 return iflag;
 }
