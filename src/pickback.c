b1001212d67e98fc3ad12ce2402dffbae207578a
ArDean Leith <leith@wadsworth.org>
2015-09-04 10:22:46 -0400

    modified  for tru, better msg, deletion

--- src/pickback.c
+++ src/pickback.c
@@ -1,5 +1,5 @@
 
-/*$Header: /usr8/web/src/RCS/pickback.c,v 1.14 2005/10/18 16:59:50 leith Exp $*/
+/*$Header: /usr8/web/src/RCS/pickback.c,v 1.15 2015/09/01 17:53:27 leith Exp $*/
 
 /*
  ***********************************************************************
@@ -8,7 +8,7 @@
  *
  ***********************************************************************
  C=* FROM: WEB - VISUALIZER FOR SPIDER MODULAR IMAGE PROCESSING SYSTEM *
- C=* Copyright (C) 1992-2005  Health Research Inc.                     *
+ C=* Copyright (C) 1992-2015  Health Research Inc.                     *
  C=*                                                                   *
  C=* HEALTH RESEARCH INCORPORATED (HRI),                               *   
  C=* ONE UNIVERSITY PLACE, RENSSELAER, NY 12144-3455.                  *
@@ -31,10 +31,9 @@
  C=* 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.     *
  C=*                                                                   *
  ***********************************************************************
- *
  * 
  * 
- * PURPOSE:     Interactive tilt-pair background selecting      
+ * PURPOSE:     Interactive tilt-pair background window selection      
  * 
  * PARAMETERS:	 
  *
@@ -46,46 +45,46 @@
 #include "common.h"
 #include "routines.h"
 
- /* internal functions */
- void           pickback_pop (Widget, XEvent *, String *, Cardinal *);
+ /* Internally defined  functions */
+ void               pickback_pop (Widget, XEvent *, String *, Cardinal *);
           
- /* externally defined file global variables */
+ /* Externally defined global variables */
+ extern char        dfil4[12], dfil5[12];
  extern int         nsam1l,nrow1l,nsam2l,nrow2l;
  extern int         nsam1r,nrow1r,nsam2r,nrow2r;
- extern int         ixull,iyull,ixulr,iyulr;
- extern char        dfil4[12], dfil5[12];
+ extern int         ixull, iyull, ixulr, iyulr;
  extern int         ixulli,iyulli,ixulri,iyulri;
- extern int         ixulw,iyulw,nsamw,nroww;
- extern int         iredu;          /* image reduction factor  */
- extern int         fitted, lbnum;
+ extern int         ixulw, iyulw, nsamw, nroww;
+ extern int         iredu;               /* Image reduction factor  */
+ extern int         fitted;
  extern float       phif, thetaf, gammaff;
- extern FILEDATA*   filedatal;
- extern FILEDATA*   filedatar;
+ extern FILEDATA *  filedatal;
+ extern FILEDATA *  filedatar;
  extern int         firstback;
  extern GC          icontxor;
- extern             int maxpart;
+ extern int         maxpart;
 
- /* internal file scope  variables */
- static int         gotright = FALSE;
+ /* Internal file scope variables */
  static int         nsamsl, nrowsl, nsamsr, nrowsr;
- static int         maxbox = 0;     /* max. box  in doc file   */
- static int         left  = TRUE;   /* start with left image   */
  static int         openitl, openitr;
+ static int         gotright = FALSE;
+ static int         left     = TRUE;    /* Start with left image   */
 
- /* global variables */
- int                numb;           /* current window number   */
- FILE    *          fpdoc4, *fpdoc5;
+ /* Global variables defined here */
+ int                back_win_now;       /* Current window number   */
+ FILE *             fpdoc4;             /* Used by: pickbackmen.c  */
+ FILE *             fpdoc5;             /* Used by: pickbackmen.c  */
 
  /***********************  pick  ***********************************/
 
  void pickback(int firstrun)
 
  {
- openitl = firstrun;
- openitr = firstrun;
+ openitl   = firstrun;
+ openitr   = firstrun;
  firstback = FALSE;
 
- /* find displayed size of both images */
+ /* Find displayed size of both images */
  nsamsl = nsam2l - nsam1l + 1;
  nsamsr = nsam2r - nsam1r + 1;
  nrowsl = nrow2l - nrow1l + 1;
@@ -94,102 +93,118 @@
  gotright = FALSE;
  left     = TRUE;
 
- /* open a message window with the following strings  */
+ /* Open a message window with the following strings  */
  showbutx("Select left background window.", 
-          "Menu.", 
+          "Show background window menu.", 
           " ", FALSE);
 
-
- /* set the actions for right, left, and center buttons */
+ /* Set the actions for right, left, and center buttons */
  actions(iw_win, "pickback_pop", pickback_pop,"M123");
  }
 
  /************************* pickback_pop **************************/
 
  void pickback_pop(Widget iw_t, XEvent *event, String *params,
-               Cardinal *num_params)
+                   Cardinal *num_params)
  {
- int           ixr,iyr, ixs, iys, ixt, iyt;
- static int    ixp, iyp;
  char          outstr[60];
  float         dlist[8];
+ int           ixr,iyr, ixs, iys, ixt, iyt;
+ static int    ixp, iyp;
  float         xt,yt,  fx,fy, aver;
 
+ char          strwin1[] = "Window Locations in Untilted File "; 
+ char          strwin2[] = "Window Locations in Tilted File "; 
+
+ char          strwin[] = 
+  "Window    Average         Reduced-X,Y-location     Columns        Rows         Flag"; 
+
  if (!(strcmp(*params, "M")))
-   {  /****************************************** mouse movement only */
+   {  // ******************************************** Mouse movement only 
+
    getloc(event,'m',&ixs,&iys);
    if (left && 
        (ixs < ixull || ixs >= ixull + nsamsl || 
         iys < iyull || iys >= iyull + nrowsl ))
-       {    /* cursor outside of displayed left image */
+       {    /* Cursor outside of displayed left image */
        spouto("*** Not in left image.");
        }
 
     else if (!left &&
        (ixs < ixulr || ixs >= ixulr + nsamsr || 
         iys < iyulr || iys >= iyulr + nrowsr ))
-       {    /* cursor outside displayed right image */
+       {    /* Cursor outside displayed right image */
        spouto("*** Not in right image.");
        }
     else
        spouto("                        ");
 
-    /* draw xor box */
+    /* Draw xor box */
     xorbox(iwin,    icontxor, FALSE, ixs, iys, nsamw, nroww);
-    xorbox(imagsav, icontxor, FALSE, ixs, iys, nsamw, nroww); 
+#ifndef WEB_TRUE    
+    xorbox(imagsav, icontxor, FALSE, ixs, iys, nsamw, nroww);
+#endif 
     } 
 
- /********************************************************left button */ 
+ // ******************************************************** Left button  
 
  else if (left && !(strcmp(*params, "1")))
-   {   /*  in left image -- button 1 pushed */
+   {   /*  Want box in left image -- button 1 pushed */
     getloc(event,'B',&ixs,&iys);
 
-    /* find location inside whole left image */
+    /* Find location inside whole left image */
     ixulw    = ixs - ixulli + 1;
     iyulw    = iys - iyulli + 1;
 
    if (ixs < ixull || ixs >= ixull + nsamsl || 
        iys < iyull || iys >= iyull + nrowsl )
-       {       /* cursor is outside displayed left image, want inside */
-       spouto("*** Not in left image.");
+       {       /* Cursor is outside displayed left image, want inside */
+       spouto("*** Not in left image."); XBell(idispl,50);
        }
 
     else 
-       {   /* want to record left background window location */
-       numb++;
+       {   /* Want to record left background window location */
        spoutfile(TRUE);
-       sprintf(outstr,"Left window: %d (%d,%d)",numb,ixulw,iyulw);
+       sprintf(outstr,"Left window:  %d (%d,%d)",back_win_now,ixulw,iyulw);
        spout(outstr);
        spoutfile(FALSE);
 
-       /* leave permanent box at this location  */
-       xorbox(iwin,    icontxor, TRUE, ixs, iys, nsamw, nroww); 
+       /* Leave permanent box at this location  */
+#ifndef WEB_TRUE    
+       xorbox(iwin,    icontxor, TRUE, ixs, iys, nsamw, nroww);
+#endif 
        xorbox(imagsav, icontxor, TRUE, ixs, iys, nsamw, nroww);  
 
-       /* find average within window */
+       /* Find average within window */
        rcaver(filedatal, nsaml, nrowl, nsamw, nroww, 
               ixulw, iyulw,  &aver);
 
-       /* save info in doc file */        
-       dlist[0] = numb;
+       /* Save info in doc file */        
+       dlist[0] = back_win_now;
        dlist[1] = aver;
        dlist[2] = ixulw * iredu;
        dlist[3] = iyulw * iredu;
        dlist[4] = nsamw;
        dlist[5] = nroww;       
        dlist[6] = 1.0;       
-       fpdoc4   = savdn1(dfil4, datexc, &fpdoc4,
-                         dlist, 7, &openitl, TRUE, TRUE);
+       if (openitl)      
+          {
+          fpdoc4 = savdnc(dfil4, datexc, &fpdoc4,
+                          dlist, 0, &openitl, TRUE, TRUE,strwin1);
+          fpdoc4 = savdnc(dfil4, datexc, &fpdoc4,
+                          dlist, 0, &openitl, TRUE, FALSE,strwin);
+          }
+       fpdoc4 = savdn1(dfil4, datexc, &fpdoc4,
+                             dlist, 7, &openitl, TRUE, FALSE);
 
-       /* find predicted location in right image */
+       /* Find predicted location in right image */
        if (fitted)
           {
-          /* transform the values */
+          /* Transform the values */
           fx = (float)ixulw;
           fy = (float)iyulw;
 
-          /* use angles to get predicted location in tilted image */
+          /* Use angles to get predicted location in tilted image */
           witran(&fx, &fy,  &xt, &yt, 1, gammaff, thetaf, phif);
 
           ixt = xt;
@@ -197,153 +212,174 @@
 
           if ((ixt < 1 || ixt > ixulr + nsamsr ||
                iyt < 1 || iyt > iyulr + nrowsr))
-             {    /* predicted  loc. is outside of right image, */
-             sprintf(outstr,"*** Tilted not in right image: (%d,%d)",
+             {    /* Predicted  loc. is outside of right image, */
+             sprintf(outstr,"*** Window not in right image: (%d,%d)",
                              ixt,iyt);
              spout(outstr);
              }
           else
-             {   /* warp cursor to predicted location on tilted side */
+             {   /* Warp cursor to predicted location on tilted side */
              ixr = ixt + ixulri;
              iyr = iyt + iyulri;
 
              movecur(ixr-ixs,iyr-iys);
 
-             /* draw box at this location */
+             /* Draw box at this location */
              xorbox(iwin,    icontxor, FALSE, ixr, iyr, nsamw, nroww);
+#ifndef WEB_TRUE    
              xorbox(imagsav, icontxor, FALSE, ixr, iyr, nsamw, nroww); 
+#endif 
              }
           }
        else
-          { /* no tilt angle available yet */
-          /* warp cursor to center of tilted side */
+          { /* No tilt angle available yet */
+
+          /* Warp cursor to center of tilted side */
           ixt = ixulr + nsamsr / 2;
           iyt = iyulr + nrowsr / 2;
 
           movecur(ixt-ixs,iyt-iys);
 
-          /* draw box at this location  */
+          /* Draw box at this location  */
           xorbox(iwin,    icontxor, TRUE, ixt, iyt, nsamw, nroww);
+#ifndef WEB_TRUE    
           xorbox(imagsav, icontxor, TRUE, ixt, iyt, nsamw, nroww);
+#endif 
           } 
 
        left     = FALSE;
        gotright = FALSE;
-       if (numb > maxbox) maxbox = numb;
 
-       /*  remove message */
+       /*  Remove message */
        showbutx("","","",TRUE);
 
-       /* open a message window with the following strings  */
+       /* Open a message window with the following strings  */
        showbutx("Select right background window.", 
-                "Menu.", 
+                "Show background window menu.", 
                 "Reselect left window.", FALSE);
 
-       /* record undo location */
+       /* Record undo location */
        ixp  = ixs;
        iyp  = iys;
        }
     }
 
  else if (!(strcmp(*params, "1")))
-    {                          /*  in right image -- button 1 pushed */
+    {             /*  Want box in right image -- Button 1 pushed */
     getloc(event,'B',&ixs,&iys);
 
-    /* find location inside whole right image */
+    /* Find location inside whole right image */
     ixulw    = ixs - ixulri + 1;
     iyulw    = iys - iyulri + 1; 
 
     if (ixs < ixulr || ixs >= ixulr + nsamsr || 
         iys < iyulr || iys >= iyulr + nrowsr )
-       {    /* cursor outside of displayed right image, want inside */
-       spouto("*** Not in right image.");
+       {    /* Cursor outside of displayed right image, want inside */
+       spouto("*** Window not in right image."); XBell(idispl,50);
        }
+ 
 
     else 
-       {   /* want to record this location */
+       {   /* Want to average this location */
        spoutfile(TRUE);
-       sprintf(outstr,"Right window:%d  (%d,%d)",numb,ixulw,iyulw);
+       sprintf(outstr,"Right window: %d  (%d,%d)",back_win_now,ixulw,iyulw);
        spout(outstr);
        spoutfile(FALSE);
 
-       /* leave permanent box at this location 
+       /* Leave permanent box at this location */ 
+#ifndef WEB_TRUE    
        xorbox(iwin,    icontxor, TRUE, ixs, iys, nsamw, nroww);
-       xorbox(imagsav, icontxor, TRUE, ixs, iys, nsamw, nroww);  */
+#endif
+       xorbox(imagsav, icontxor, TRUE, ixs, iys, nsamw, nroww);   
 
-       /* find average within window */
+       /* Find average within window */
        rcaver(filedatar, nsamr, nrowr, nsamw, nroww,
               ixulw, iyulw,  &aver);
 
-
-       /* save info in doc file */ 
-       dlist[0] = numb;
+       /* Save info in doc file */ 
+       dlist[0] = back_win_now;
        dlist[1] = aver;
        dlist[2] = ixulw * iredu;
        dlist[3] = iyulw * iredu;
        dlist[4] = nsamw;
        dlist[5] = nroww;       
-       dlist[6] = 1.0;       
-       fpdoc5   = savdn1(dfil5, datexc, &fpdoc5,
-                         dlist, 7, &openitr, TRUE, TRUE);
-
-       /*  remove message */
+       dlist[6] = 1.0;  
+       if (openitr)      
+          {
+          fpdoc5 = savdnc(dfil5, datexc, &fpdoc5,
+                           dlist, 0, &openitr, TRUE, TRUE, strwin2);
+          fpdoc5 = savdnc(dfil5, datexc, &fpdoc5,
+                           dlist, 0, &openitr, TRUE, FALSE,strwin);
+          }
+       fpdoc5 = savdn1(dfil5, datexc, &fpdoc5,
+                       dlist, 7, &openitr, TRUE, FALSE);
+      
+       /*  Remove button message */
        showbutx("","","",TRUE);
 
-       /* open a message window with the following strings  */
+       /* Open a button message window with following strings */
        showbutx("Select left background window.", 
-                "Menu.", 
+                "Show background window menu.", 
                 "Reselect right window.", FALSE);
 
        left     = TRUE;
        gotright = TRUE;
-       if (numb > maxbox) maxbox = numb;
+       back_win_now++;
 
-       /* warp cursor to last position of untilted side */
-        movecur(ixp-ixs,iyp-iys);
+       // Update widgit for back_win_now
+       pickbackmen();
 
-       /* record undo location */
+       /* Warp cursor to last position of untilted side */
+       movecur(ixp-ixs,iyp-iys);
+
+       /* Record undo location */
        ixp  = ixs;
        iyp  = iys;
 
-       /* draw box at previous location on left side */  
+       /* Draw box at previous location on left side */  
        xorbox(iwin,    icontxor, TRUE, ixp, iyp, nsamw, nroww);
-       xorbox(imagsav, icontxor, TRUE, ixp, iyp, nsamw, nroww); 
+#ifndef WEB_TRUE    
+       xorbox(imagsav, icontxor, TRUE, ixp, iyp, nsamw, nroww);
+#endif 
        }
     }
 
- /***************************************************** middle button */ 
+ /***************************************************** Middle button */ 
 
  else if (!(strcmp(*params, "2")))
-    {                          /* show menu --       button 2 pushed */
-    lbnum = numb;
-    fitsav(maxpart);
-
+    {     /* Show menu --       Button 2 pushed */
     pickbackmen();
     }
 
- /****************************************************** right button */ 
+ /****************************************************** Right button */ 
 
  else if (!left && !(strcmp(*params, "3")))
 
-    {                          /*  in right image -- button 3 pushed */
+    {     /*  In right image -- Button 3 pushed */
     getloc(event,'B',&ixs,&iys);
 
-    /* warp cursor back to left image location */
+    /* Warp cursor back to left image location */
     spout("Moving cursor back to left");
     movecur(ixp-ixs, iyp-iys);
  
-    /* draw box at previous location on  other side  */ 
+    /* Draw box at previous location on  other side  */ 
     xorbox(iwin,    icontxor, FALSE, ixp, iyp, nsamw, nroww);
+#ifndef WEB_TRUE    
     xorbox(imagsav, icontxor, FALSE, ixp, iyp, nsamw, nroww); 
+#endif 
 
-    numb--;
-   left = TRUE;
+    back_win_now--;
+
+    /* Update text box for window key number */
+    pickbackmen();
+
+    left = TRUE;
     ixp = ixs; iyp = iys;
     }
 
  else if (!(strcmp(*params, "3")))
-    {                          /*  in left image -- button 3 pushed */
-    /* warp cursor back to previous right position */
+    {                          /*  In left image -- button 3 pushed */
+    /* Warp cursor back to previous right position */
     spout("Moving cursor back to right");
 
     if (gotright) 
@@ -352,9 +388,12 @@
        left = FALSE;
 
        movecur(ixp-ixs, iyp-iys);
-       /* draw box at previous location on  other side */ 
+
+       /* Draw box at previous location on  other side */ 
        xorbox(iwin,    icontxor, FALSE, ixp, iyp, nsamw, nroww);
+#ifndef WEB_TRUE    
        xorbox(imagsav, icontxor, FALSE, ixp, iyp, nsamw, nroww); 
+#endif 
        ixp  = ixs; iyp = iys;
        }
     }             
