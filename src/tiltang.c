874b88a2d3ad57cd7f0b1d0b278c83c46eb97977
ArDean Leith <leith@wadsworth.org>
2015-09-04 10:22:57 -0400

    new output msg

--- src/tiltang.c
+++ src/tiltang.c
@@ -1,8 +1,8 @@
 
-/*$Header: /usr8/web/src/RCS/tiltang.c,v 1.14 2015/06/11 13:27:53 leith Exp $*/
+/*$Header: /usr8/web/src/RCS/tiltang.c,v 1.15 2015/09/01 17:53:42 leith Exp $*/
 
 /*
- ***********************************************************************
+ C**********************************************************************
  C                                                                     *
  C tiltang.c                                                           *
  C               Improved                        Jun 2015 ArDean Leith *
@@ -30,7 +30,7 @@
  C=* Free Software Foundation, Inc.,                                   *
  C=* 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.     *
  C=*                                                                   *
- ***********************************************************************
+ C**********************************************************************
  C
  C   tiltang(x0,y0,xt,yt,npointss,theta,iarea,arealim,wantmsg)
  C
@@ -44,140 +44,109 @@
  C  
  C   ALSO RETURNS:  theta      Computed tilt angle
  C                             (Theta only altered if ok)
- C                  iarea      Number of tiangles used to get tilt             
+ C                  iarea      Number of triangles used to get tilt             
  C                         
- **********************************************************************
+ C*********************************************************************
 */
 
 #include "common.h"
 #include "routines.h"
 
-int tiltang(float *x0, float *y0, float *xt, float *yt,
-	    int npoints, float * theta, 
-            int * iarea, float arealim, int wantmsg)
+ int tiltang(float *x0,   float *y0, 
+             float *xt,   float *yt,
+	     int npoints, float * theta, 
+             int * iarea, float arealim, int wantmsg)
+ {
+ char        outmsg[80];
+ int         i, k, ntot, iflag;
+ float	     areau, areat, w, sum, temp;
+ float       x01, x02, y01, y02, xt1, xt2, yt1, yt2;
+ int         nbad = 0;
 
-{
-char	    outmsg[80];
-int	    i, k, ntot, iflag;
-float	    area0, areat, w, sum, temp;
-float       x01, x02, y01, y02, xt1, xt2, yt1, yt2;
+ const       float pi = 3.14159;
 
-const       float pi = 3.14159;
+ sum    = 0.0;
+ *iarea = 0;
+ ntot   = 0;
 
-sum    = 0.0;
-*iarea = 0;
-ntot   = 0;
+ if (npoints < 3)
+    { 
+                  //123456789 123456789 123456789 123456789 123456789 123456789 1234567890
+    sprintf(outmsg,"*** Unable to compute tilt;  Need > 2 points, Have only: %d", npoints); 
+    spout(outmsg); 
+    XBell(idispl,50);
+    return 1;
+    }
 
-if (npoints < 3)
-   { 
-   sprintf(outmsg,"*** Unable to compute tilt (theta); Have only: %d points.  Need > 2 points",npoints); 
-   spout(outmsg); 
-   XBell(idispl,50);
-   return 1;
-   }
+ /* Set good locations return flag */
+ iflag = 0;
 
-iflag = 0;
-for (i = 0; i < npoints; i++)
+ for (i = 0; i < npoints; i++)
     {
     for (k = i + 1; k < npoints-1; k++)
 	{
-        /* Area in untilted image: */
+        /* Find area in untilted image: */
 	x01    = x0[k]   - x0[i];
 	y01    = y0[k]   - y0[i];
 	x02    = x0[k+1] - x0[i];
 	y02    = y0[k+1] - y0[i];
-	area0  = (float) fabs( (double)(x01 * y02 - x02 * y01));
+	areau  = (float) fabs( (double)(x01 * y02 - x02 * y01));
+
         ntot++;
 
-        /* Default arealim is 5000 sq. pixels */
-	if (area0 >= arealim)
-	    {  /* only triangles > arealim are considered */
-            /* area in tilted image: */
-	    xt1   = xt[k]   - xt[i];
-	    yt1   = yt[k]   - yt[i];
-	    xt2   = xt[k+1] - xt[i];
-	    yt2   = yt[k+1] - yt[i];
-            areat =  (float) fabs( (double)(xt1 * yt2 - xt2 * yt1));
+//printf("*** Triangle: %d, %d, & %d  Area: %f \n", i+1, k+1, k+2, areau ); 
 
-	    if (areat >= arealim)
-		{
-                /* Area in tilted image should be <= area in untilted */ 
-		w = areat / area0;
- 
-		if (w <= 1.0)
-		   {
-		   *theta    = acos(w);
-		   sum       = sum + *theta;
-		   (*iarea)++;
 
-                   /***** Output removed
-		   temp      = *theta / pi * 180;
-	           sprintf(outmsg,
-                     " %6dth tilt angle: %5.1f  Area: %8.1f ---> %8.1f",
-		      *iarea,temp,area0,areat);
-		   spout(outmsg);
-                   ***********/
-		   }
-		else
-		   { /* Set bad location return flag */
-                   iflag = -1;
-		   sprintf(outmsg,
-                     "*** Check keys: %4d, %4d & %d  for a bad point",
-			i+1, k+1, k+2); 
-		   printf("%s \n",outmsg);
-		   spout(outmsg);
-       	             
-                   /* ***************** removed output
-		   spout("*** COORDINATES IN UNTILTED IMAGE:");
-		   sprintf(outmsg, " (%7.2f,%7.2f) (%7.2f,%7.2f) (%7.2f,%7.2f)",
-			   x0[i],y0[i], x0[k],y0[k], x0[k+1],y0[k+1]);
-		   printf("%s \n",outmsg);
-                   spout(outmsg);
+        /* Default arealim is 5000 square pixels */
+	if (areau >= arealim)
+	   {  /* Only triangles > arealim are considered */
 
-  		   spout("COORDINATES IN TILTED IMAGE:");
-		   sprintf(outmsg, " (%7.2f,%7.2f) (%7.2f,%7.2f) (%7.2f,%7.2f)",
-                            xt[i],yt[i], xt[k],yt[k], xt[k+1],yt[k+1]);
-		   printf("%s\n",outmsg);
- 		   spout(outmsg); 
-                   sprintf(outmsg,
-                   "Untilted Area: %8.1f < tilted area:  %8.1f",area0,areat);
-		   printf("%s\n",outmsg);
-	           spout(outmsg);
-    
-		   spout("*** WARNING, ARGUMENT OF ARCCOS > 1"); 
-		   spout("DIFFERENCE VECTORS: ");
-		   sprintf(outmsg, " %7.2f %7.2f %7.2f %7.2f",x01,y01, x02,y02);
-		   printf("%s\n",outmsg);
-		   spout(outmsg);
+           /* Find area in tilted image: */
+           xt1   = xt[k]   - xt[i];
+           yt1   = yt[k]   - yt[i];
+           xt2   = xt[k+1] - xt[i];
+           yt2   = yt[k+1] - yt[i];
+           areat = (float) fabs( (double)(xt1 * yt2 - xt2 * yt1));
 
-		   spout("DIFFERENCE VECTORS:"); 
-		   sprintf(outmsg, "%7.2f %7.2f %7.2f %7.2f", xt1,yt1, xt2,yt2);
-		   printf("%s\n",outmsg);
- 		   spout(outmsg);
-                   ***************** */
-		   }
-		}
-	    }
-	}
+	   if (areat >= arealim)
+              {
+              /* Area in tilted image should be <= area in untilted */ 
+              w = areat / areau;
+
+              if (w > 1.01)
+                 {     /* Set bad location return flag */
+                 iflag = -1;
+                 //printf("*** Areas: %6.2f, %6.2f  Lim: %6.2f \n", areau,areat,arealim); 
+                 printf("*** Check points: %4d, %4d & %d  for a bad value \n", i+1, k+1, k+2); 
+                 }
+	      else
+		 {
+		 *theta = acos(w);
+		 sum    = sum + *theta;
+		 (*iarea)++;
+		 }
+	      }   // End of: if (areat >= arealim)
+           }      // End of: if (areau >= arealim)
+        }         // End of: for (k = i + 1; k < npoints-1; k++)
+    }             // End of: for (i = 0; i < npoints; i++)
+
+ if (*iarea == 0) 
+    {
+    spout(" *** Unable to compute tilt angle; Need > 2 points with area > arealim!");
+    spout(" *** Choose points that are more triangular in distribution!");
+    return 1;
     }
 
-if (*iarea == 0) 
-   {
-   spout(" *** Unable to compute tilt angle; Need > 2 points with area > arealim!");
-   return 1;
-   }
+ *theta = sum    / (*iarea);
+ *theta = *theta / pi * 180.0;
 
-*theta = sum    / (*iarea);
-*theta = *theta / pi * 180.0;
+ if (wantmsg)
+    {
+    sprintf(outmsg, "Points: %d   Areas used for theta: %d, out of possible: %d", 
+                    npoints, *iarea, ntot);
+    spout(outmsg);
+    } 
 
-if (wantmsg)
-   {
-   sprintf(outmsg, "Points: %d   Areas used for theta: %d, out of possible: %d", 
-                 npoints, *iarea, ntot);
-   spout(outmsg);
-   } 
-
-return iflag;
-}
-
+ return iflag;
+ }
                       
