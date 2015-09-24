f9d5272ac8edc9c10e7065482c56d9e37c3b3ca3
ArDean Leith <agleith@gmail.com>
2015-09-24 11:27:23 -0400

    Parameters changed to select which files affected

--- src/fitdoc.c
+++ src/fitdoc.c
@@ -1,5 +1,5 @@
 
-/*$Header: /usr8/web/src/RCS/fitdoc.c,v 1.33 2015/09/01 17:52:51 leith Exp $*/
+/*$Header: /usr8/web/src/RCS/fitdoc.c,v 1.35 2015/09/22 14:21:53 leith Exp $*/
 
 /*
  C**********************************************************************
@@ -7,6 +7,8 @@
  C  fitdoc.c
  C           Improved                            Jun 2015 ArDean Leith  
  C           Added fitdoc_addpart                Aug 2015 ArDean Leith  
+ C           If (maxpart1 <= 0 ) paranoia        Sep 2015 ArDean Leith   
+ C           Parameters changed                  Sep 2015 ArDean Leith   
  C
  C**********************************************************************
  C=* FROM: WEB - VISUALIZER FOR SPIDER MODULAR IMAGE PROCESSING SYSTEM *
@@ -89,10 +91,13 @@
 
  /********************   fitdoc   ****************************/
 
- int fitdoc(int nofile_msg)
+ int fitdoc(Boolean want_nofile_msg, 
+            Boolean get_loc_doc, 
+            Boolean get_tilt_doc, 
+            Boolean get_win_doc)
 
  { 
- int      i,k, maxpart1,maxpart2;
+ int      i,k, iredut, maxpart1,maxpart2;
  int      maxwin1,maxwin2;
  int      back_wins1,back_wins2;
  float  * ptr;
@@ -101,205 +106,217 @@
  int      maxkeys = 9999;  /* Set size for array pointed to by dbuf */
  int      isok    = FALSE;
 
- /* Retrieve data from dfil1 for untilted points */
- isok = getdoc_f( (FILE *) NULL, dfil1, datexc,maxkeys, maxreg, &dbuf, &maxpart1, FALSE);
+ if (get_loc_doc) 
+    {
+    /* Retrieve data from dfil1 for untilted points ---------------------------------- */
+    isok = getdoc_f( (FILE *) NULL, dfil1, datexc,maxkeys, maxreg, &dbuf, &maxpart1, FALSE);
 
- if ( (xim == NULL) || (maxpart1 > sizepartarray) )
-    {           /* Create initial or larger particle location arrays */
-    sizepartarray = maxpart1 + 300;
+    if ( maxpart1 <= 0 ) maxpart1 = 0;   // Error recovery
 
-    if (((xim = (float *) realloc((void *) xim, sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((xu0 = (float *) realloc((void *) xu0, sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((yu0 = (float *) realloc((void *) yu0, sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((xs =  (float *) realloc((void *) xs,  sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((ys =  (float *) realloc((void *) ys,  sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((xs2 = (float *) realloc((void *) xs2, sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((ys2 = (float *) realloc((void *) ys2, sizepartarray * sizeof(float))) == (float *) NULL))
-        { spout("*** Unable to reallocate xim memory in fitdoc."); return 1; }
-    }
+    if ( xim == NULL || maxpart1 > sizepartarray || maxpart1 == 0 )
+       {           /* Create initial or larger particle location arrays */
+       sizepartarray = maxpart1 + 300;
 
- if (isok != 0) 
-    {         /* Problem retrieving doc file. Assume it does not exist */
+       if (((xim = (float *) realloc((void *) xim, sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((xu0 = (float *) realloc((void *) xu0, sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((yu0 = (float *) realloc((void *) yu0, sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((xs =  (float *) realloc((void *) xs,  sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((ys =  (float *) realloc((void *) ys,  sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((xs2 = (float *) realloc((void *) xs2, sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((ys2 = (float *) realloc((void *) ys2, sizepartarray * sizeof(float))) == (float *) NULL))
+           { spout("*** Unable to reallocate memory in fitdoc."); return 1; }
+       }
+
+    if (isok != 0) 
+       {         /* Problem retrieving doc file. Assume it does not exist */
+       if (dbuf) free(dbuf); dbuf = NULL;
+       if ( want_nofile_msg )
+          {spouts("*** Unable to read untilted doc file: "); spouti(isok); spout(dfil1);} 
+       maxpart = 0;
+       return   -1;
+       }
+
+    /* Fill xim, xu0, yu0 arrays */
+    ptr = dbuf;
+    for (k=0; k<maxpart1 ; k++)
+       {
+       xim[k]  = *(ptr+1);  
+       xu0[k]  = *(ptr+4);
+       yu0[k]  = *(ptr+5);  
+       ptr    += maxreg; 
+       }
+
+    /* Free dbuf here*/
     if (dbuf) free(dbuf); dbuf = NULL;
-    if ( !(nofile_msg) )
-       {spouts("*** Unable to read untilted doc file: "); spouti(isok); spout(dfil1);} 
-    maxpart = 0;
-    return   -1;
-    }
 
- /* Fill xim, xu0, yu0 arrays */
- ptr = dbuf;
- for (k=0; k<maxpart1 ; k++)
-    {
-    xim[k]  = *(ptr+1);  
-    xu0[k]  = *(ptr+4);
-    yu0[k]  = *(ptr+5);  
-    ptr    += maxreg; 
-    }
+    /* Retrieve data from dfil2 for tilted points -------------------------------- */
+    if (getdoc_f((FILE *) NULL, dfil2, datexc,maxkeys, maxreg, &dbuf, &maxpart2, FALSE) > 0) 
+       {   /* Problem retrieving doc file, assume it does not exist */
+       if (dbuf) free(dbuf); dbuf = NULL;
+       if ( want_nofile_msg )
+          {spouts("*** Unable to read tilted doc file: "); spout(dfil2);} 
+       maxpart = 0;
+       return   -2;
+       }
 
- /* Free dbuf here*/
- if (dbuf) free(dbuf); dbuf = NULL;
+    if (  maxpart2 > sizepartarray )
+       {           /* Create initial or larger particle location arrays */
+       sizepartarray = maxpart2 + 300;
 
+       if (((xim = (float *) realloc((void *) xim, sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((xu0 = (float *) realloc((void *) xu0, sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((yu0 = (float *) realloc((void *) yu0, sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((xs =  (float *) realloc((void *) xs,  sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((ys =  (float *) realloc((void *) ys,  sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((xs2 = (float *) realloc((void *) xs2, sizepartarray * sizeof(float))) == (float *) NULL) ||
+           ((ys2 = (float *) realloc((void *) ys2, sizepartarray * sizeof(float))) == (float *) NULL))
+           { spout("*** Unable to reallocate memory in fitdoc."); return 1; }
+       }
 
- /* Retrieve data from dfil2 for tilted points */
- if (getdoc_f((FILE *) NULL, dfil2, datexc,maxkeys, maxreg, &dbuf, &maxpart2, FALSE) > 0) 
-    {   /* Problem retrieving doc file, assume it does not exist */
+    /* Fill xs, ys arrays */
+    ptr = dbuf;
+    for (k=0; k < maxpart2 ; k++)
+       { 
+       xs[k]  = *(ptr+4); 
+       ys[k]  = *(ptr+5);  
+       ptr   += maxreg;
+       }
+
+    /* Free dbuf here*/
     if (dbuf) free(dbuf); dbuf = NULL;
-    if ( !(nofile_msg) )
-       {spouts("*** Unable to read tilted doc file: "); spout(dfil2);} 
-    maxpart = 0;
-    return   -2;
-    }
 
- if (  maxpart2 > sizepartarray )
-    {           /* Create initial or larger particle location arrays */
-    sizepartarray = maxpart2 + 300;
+    if (maxpart2 < maxpart1)
+       {
+       /* Tilted file has more points than first! */  
+       spout("*** Tilted doc file has fewer particles than untilted.");
+       maxpart = 0;
+       return 1;
+       }
 
-    if (((xim = (float *) realloc((void *) xim, sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((xu0 = (float *) realloc((void *) xu0, sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((yu0 = (float *) realloc((void *) yu0, sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((xs =  (float *) realloc((void *) xs,  sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((ys =  (float *) realloc((void *) ys,  sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((xs2 = (float *) realloc((void *) xs2, sizepartarray * sizeof(float))) == (float *) NULL) ||
-        ((ys2 = (float *) realloc((void *) ys2, sizepartarray * sizeof(float))) == (float *) NULL))
-        { spout("*** Unable to reallocate xim memory in fitdoc."); return 1; }
-    }
-
- /* Fill xs, ys arrays */
- ptr = dbuf;
- for (k=0; k < maxpart2 ; k++)
-    { 
-    xs[k]  = *(ptr+4); 
-    ys[k]  = *(ptr+5);  
-    ptr   += maxreg;
-    }
-
- /* Free dbuf here*/
- if (dbuf) free(dbuf); dbuf = NULL;
-
- if (maxpart2 < maxpart1)
-    {
-    /* Tilted file has more points than first! */  
-    spout("*** Tilted doc file has fewer particles than untilted.");
-    maxpart = 0;
-    return 1;
-    }
-
- if (maxpart2 > maxpart1)
-    {
-    /* Tilted file has more points than first! */  
-    spout("*** Tilted doc file has more particles than untilted.");
-    maxpart = 0;
-    return 1;
-    }
+    if (maxpart2 > maxpart1)
+       {
+       /* Tilted file has more points than first! */  
+       spout("*** Tilted doc file has more particles than untilted.");
+       maxpart = 0;
+       return 1;
+       }
  
- maxpart = maxpart1;
+    maxpart = maxpart1;
+    }  // End of: if (get_loc_doc)
  
- /* Retrieve data from dfil3 for tilt parameters (if any) */
- /* (x,y) coordinates of origin and tilt angles delta, phi ,gamma */
- 
- /* Read the maxpart from dcb*** file */
- maxkeys = 125;      /* Set size for array pointed to by dbuf */
- if (getdoc_f((FILE *) NULL, dfil3, datexc, maxkeys, maxreg, &dbuf, &maxtilts, FALSE) > 1) 
-    {   /* Problem retrieving doc file. Assume it does not exist */
-    if (dbuf) free(dbuf); dbuf = NULL;
-    if ( !(nofile_msg))
-       {spouts("*** Tilt angle doc file not available yet: "); spout(dfil3);} 
-    return 0;
-    }
-
- if (maxtilts < 124)
+ if (get_tilt_doc) 
     {
-    spout(" Tilt angle doc. file lacks necessary info.");
-    if (dbuf) free(dbuf); dbuf = NULL;
-    return 1;
-    }
+    /* Retrieve data from dfil3 for tilt parameters (if any) ------------------------------- */
+    /* Parametes: Coordinates of origin and tilt angles: theta, phi ,gamma */
+ 
+    maxkeys = 125;      /* Set size for array pointed to by dbuf */
+    if (getdoc_f((FILE *) NULL, dfil3, datexc, maxkeys, maxreg, &dbuf, &maxtilts, FALSE) > 1) 
+       {   /* Problem retrieving doc file. Assume it does not exist */
+       if (dbuf) free(dbuf); dbuf = NULL;
+       if ( want_nofile_msg)
+          {spouts("*** Tilt angle doc file not available yet: "); spout(dfil3);} 
+       return 0;
+       }
 
- /* Get last particle number */
- ptr          = dbuf + ((121-1) * maxreg);
- limfit       = *(ptr+5);        /* Number of markers used in fitting */
+    if (maxtilts < 124)
+       {
+       spout(" Tilt angle doc. file lacks necessary info.");
+       if (dbuf) free(dbuf); dbuf = NULL;
+       return 1;
+       }
 
- /* Get fitted flag  */
- ptr      = dbuf + ((122-1) * maxreg);
- fitted   = *(ptr+1);
+    /* Get last particle number */
+    ptr    = dbuf + ((121-1) * maxreg);
+    limfit = *(ptr+5);        /* Number of locations used in fitting */
 
- /* Get origin */
- ptr      = dbuf + ((123-1) * maxreg);
- xu0t     = *(ptr+1);
- yu0t     = *(ptr+2);
- xs0t     = *(ptr+3);
- ys0t     = *(ptr+4);
- iredu    = *(ptr+5);
+    /* Get fitted flag  */
+    ptr    = dbuf + ((122-1) * maxreg);
+    fitted = *(ptr+1);
 
- /* Get tilt angles */
- ptr      = dbuf + ((124-1) * maxreg);
- thetaf   = *(ptr+1);
- gammaff  = *(ptr+2);
- phif     = *(ptr+3);
+    /* Get origin */
+    ptr    = dbuf + ((123-1) * maxreg);
+    xu0t   = *(ptr+1);
+    yu0t   = *(ptr+2);
+    xs0t   = *(ptr+3);
+    ys0t   = *(ptr+4);
+    iredut = *(ptr+5);
+
+    if (iredut != iredu)
+       {
+       spouts("Inconsistent image size reduction factor: "); spouti(iredut) ;
+       if (dbuf) free(dbuf); dbuf = NULL;
+       return 1;
+       }
+
+    /* Get tilt angles */
+    ptr     = dbuf + ((124-1) * maxreg);
+    thetaf  = *(ptr+1);
+    gammaff = *(ptr+2);
+    phif    = *(ptr+3);
 	      
- /* Free dbuf here */
- if (dbuf) free(dbuf); dbuf = NULL;
+    /* Free dbuf here */
+    if (dbuf) free(dbuf); dbuf = NULL;
+    }  // End of: if (get_tilt_doc)
 
 
- /* Retrieve data from dfil4 & dfil5 for # of background windows (if any) */
+ if (get_win_doc)
+    {
+    /* Retrieve data from dfil4 & dfil5 for # of background windows (if any) --------------- */
  
- back_win_now = 0;        /* Used for pickback */
+    back_win_now = 1;        /* Used for pickback */
 
- maxkeys = 9999;          /* Set size for array pointed to by dbuf */
+    maxkeys = 9999;          /* Set size for array pointed to by dbuf */
 
- if (getdoc_f((FILE *) NULL, dfil4, datexc, maxkeys, maxreg, &dbuf, &maxwin1, FALSE) > 1) 
-    {   /* Problem retrieving doc file. Assume it does not exist */
+    if (getdoc_f((FILE *) NULL, dfil4, datexc, maxkeys, maxreg, &dbuf, &maxwin1, FALSE) > 1) 
+       {   /* Problem retrieving doc file. Assume it does not exist */
+       if (dbuf) free(dbuf); dbuf = NULL;
+       //{spouts("*** Background window doc file not available yet: "); spout(dfil4);} 
+       return 0;
+       }
+
+    back_wins1 = 0;
+    ptr        = dbuf;
+    for (i= 0; i < maxwin1; i++)
+       {
+       //if (i < 20) printf("at ptr:   %f %f \n",*ptr,*(ptr+1));
+       if (*ptr > 0 && i < 998) back_wins1 = i + 1; 
+       ptr += maxreg;
+       }
+
+    /* Free dbuf here */
     if (dbuf) free(dbuf); dbuf = NULL;
-    if ( !(nofile_msg))
-       {spouts("*** Backround window doc file not available yet: "); spout(dfil4);} 
-    return 0;
-    }
- //printf("maxwin1: %d \n",maxwin1);
-
- back_wins1 = 0;
- ptr        = dbuf;
- for (i= 0; i < maxwin1; i++)
-    {
-    //if (i < 20) printf("at ptr:   %f %f \n",*ptr,*(ptr+1));
-    if (*ptr > 0 && i < 998) back_wins1 = i + 1; 
-    ptr += maxreg;
-    }
- //printf("back_wins1:   %d   \n",back_wins1);
-
- /* Free dbuf here */
- if (dbuf) free(dbuf); dbuf = NULL;
 
 
- if (getdoc_f((FILE *) NULL, dfil5, datexc, maxkeys, maxreg, &dbuf, &maxwin2, FALSE) > 1) 
-    {   /* Problem retrieving doc file. Assume it does not exist */
+    if (getdoc_f((FILE *) NULL, dfil5, datexc, maxkeys, maxreg, &dbuf, &maxwin2, FALSE) > 1) 
+       {   /* Problem retrieving doc file. Assume it does not exist */
+       if (dbuf) free(dbuf); dbuf = NULL;
+       // {spouts("*** Background window doc file not available yet: "); spout(dfil5);} 
+       return 0;
+       }
+
+    back_wins2 = 0;
+    ptr        = dbuf;
+    for (i= 0; i < maxwin1; i++)
+       {
+       if (*ptr > 0 && i < 998) back_wins2 = i + 1; 
+       ptr += maxreg;
+       }
+
+    /* Free dbuf here */
     if (dbuf) free(dbuf); dbuf = NULL;
-    if ( !(nofile_msg))
-       {spouts("*** Backround window doc file not available yet: "); spout(dfil5);} 
-    return 0;
-    }
 
- back_wins2 = 0;
- ptr        = dbuf;
- for (i= 0; i < maxwin1; i++)
-    {
-    if (*ptr > 0 && i < 998) back_wins2 = i + 1; 
-    ptr += maxreg;
-    }
+    if (back_wins1 != back_wins2) 
+       {
+       spouts("*** Number of Untilted and tilted background windows not same in: "); 
+       spouts(dfil4);  spouts(" & ");  spout(dfil5); 
+       }
 
- /* Free dbuf here */
- if (dbuf) free(dbuf); dbuf = NULL;
-
- if (back_wins1 != back_wins2) 
-    {
-    spouts("*** Number of Untilted and tilted backround windows not same in: "); 
-    spouts(dfil4);  spouts(" & ");  spout(dfil5); 
-    }
-
- /* Used for pickback */
- back_win_now    = back_wins1;         
- if (back_wins2 > back_win_now) 
-    back_win_now = back_wins2;        
- back_win_now++;         
+    /* Used for pickback */
+    back_win_now    = back_wins1;         
+    if (back_wins2 > back_win_now) 
+       back_win_now = back_wins2;        
+    back_win_now++;         
+    }  // End of: if (get_win_doc)
 
  return 0;
  }
@@ -342,3 +359,4 @@
 
  return 0;
  }
+
